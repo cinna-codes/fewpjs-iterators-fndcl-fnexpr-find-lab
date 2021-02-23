51639342f3b08f96cc2298be90d250d1f6222c59
cinna-codes <somethingfellipickeditup@gmail.com>
2021-02-23 16:14:52 -0500

    commit tree e35099883b7c800711a134412f1e943d8d149fda

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-
+superbowlWin = (record) => {
+    let result = record.find( record => record.result === "W" ) // `record.find` will just return the matching record in question. you cannot change this. do not spend 50 minutes trying to fight this fact.
+    return !!result ? result.year : undefined // `!!result` refers to the boolean value of `result`, AKA its truthy or falsyness. if it's TRUTHY, return `result.year`; if it's FALSY, return `undefined`
+  }
